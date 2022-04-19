772df72120077a191d3f5d66efd6cb4a8a560af2
primoztrubar <104010702+primoztrubar@users.noreply.github.com>
2022-04-19 17:38:17 +0200

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
 public class ChatServer {
 
 	protected int serverPort = 1234;
 	protected List<Socket> clients = new ArrayList<Socket>(); // list of clients
+	public Map<String, Socket> names = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
 		new ChatServer();
@@ -65,9 +68,57 @@ public class ChatServer {
 		}
 	}
 
-	public void removeClient(Socket socket) {
+	public void sporociNapako(Socket socket, String napaka) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("napaka", napaka);
+		String json = jsonObject.toJSONString();
+		try {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out.writeUTF(json);
+		}
+		catch (Exception e) {
+			e.printStackTrace(System.err);
+			System.err.println("[system] prišlo je do napake med pošiljanjem napake");
+		}
+	}
+
+	public void posljiPrivatnoSporocilo(Socket s, String od, String za, String sporocilo) {
+		JSONObject jsonObject = new JSONObject();	
+		try {
+			jsonObject.put("tipSporocila", "privatno");
+			jsonObject.put("od", od);
+			jsonObject.put("sporocilo", sporocilo);
+			if (!names.containsKey(za)) {
+				sporociNapako(s, "Prejemnik privatnega sporočila ne obstaja");
+				return;
+			}
+			DataOutputStream out = new DataOutputStream(names.get(za).getOutputStream());
+			out.writeUTF(jsonObject.toJSONString());
+		} catch (Exception e) {
+			System.err.println("[system] pošiljanje privatnega sporočila je spodletelo");
+			e.printStackTrace(System.err);
+		}
+	}
+
+	public void posljiJavnoSporocilo(String od, String sporocilo) {
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("tipSporocila", "javno");
+			jsonObject.put("od", od);
+			jsonObject.put("sporocilo", sporocilo);
+			sendToAllClients(jsonObject.toJSONString());
+		} catch (Exception e) {
+			System.err.println("[system] pošiljanje javnega sporočila je spodletelo");
+			e.printStackTrace(System.err);
+		}
+	}
+
+	public void removeClient(Socket socket, String name) {
 		synchronized(this) {
 			clients.remove(socket);
+			if (name != null && names.containsKey(name)) {
+				names.remove(name);
+			}
 		}
 	}
 }
@@ -75,10 +126,13 @@ public class ChatServer {
 class ChatServerConnector extends Thread {
 	private ChatServer server;
 	private Socket socket;
+	private JSONParser parser;
+	private String name;
 
 	public ChatServerConnector(ChatServer server, Socket socket) {
 		this.server = server;
 		this.socket = socket;
+		this.parser = new JSONParser();
 	}
 
 	public void run() {
@@ -90,9 +144,48 @@ class ChatServerConnector extends Thread {
 		} catch (IOException e) {
 			System.err.println("[system] could not open input stream!");
 			e.printStackTrace(System.err);
-			this.server.removeClient(socket);
+			this.server.removeClient(socket, null);
 			return;
 		}
+
+		// Dokler ni prijave
+		while (true) {
+			String msg_received;
+			try {
+				msg_received = in.readUTF();
+			} catch (Exception e) {
+				System.err.println("[system] there was a problem while reading message client on port " + this.socket.getPort() + ", removing client");
+				e.printStackTrace(System.err);
+				this.server.removeClient(this.socket, null);
+				return;
+			}
+			if (msg_received.length() == 0) // invalid message
+				continue;
+			
+			JSONObject jsonObject;
+			try {
+				jsonObject = (JSONObject) parser.parse(msg_received);
+			}
+			catch (Exception e) {
+				e.printStackTrace(System.err);
+				System.out.println("Neveljavno sporočilo.");
+				server.sporociNapako(socket, "Neveljavno sporočilo.");
+				continue;
+			}
+			if (jsonObject.containsKey("prijava")) {
+				name = (String) jsonObject.get("prijava");
+				if (server.names.containsKey(name)) {
+					server.sporociNapako(socket, "to ime že obstaja");
+					continue;
+				}
+				server.names.put(name, socket);
+				System.out.println("nova prijava: " + name);
+				break;
+			}
+			else {
+				server.sporociNapako(socket, "prijava je potrebna");
+			}
+		}		
 
 		while (true) { // infinite loop in which this thread waits for incoming messages and processes them
 			String msg_received;
@@ -101,23 +194,52 @@ class ChatServerConnector extends Thread {
 			} catch (Exception e) {
 				System.err.println("[system] there was a problem while reading message client on port " + this.socket.getPort() + ", removing client");
 				e.printStackTrace(System.err);
-				this.server.removeClient(this.socket);
+				this.server.removeClient(this.socket, name);
 				return;
 			}
-
 			if (msg_received.length() == 0) // invalid message
 				continue;
-
-			System.out.println("[RKchat] [" + this.socket.getPort() + "] : " + msg_received); // print the incoming message in the console
-
-			String msg_send = "someone said: " + msg_received.toUpperCase(); // TODO
+			
+			JSONObject jsonObject;
+			try {
+				jsonObject = (JSONObject) parser.parse(msg_received);
+				if (!jsonObject.containsKey("tipSporocila") || !jsonObject.containsKey("sporocilo")) {
+					throw new Exception("Nujno potrebni podatki v sporočilu manjkajo");
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace(System.err);
+				System.out.println("Neveljavno sporočilo.");
+				server.sporociNapako(socket, "Neveljavno sporočilo.");
+				continue;
+			}
 
 			try {
-				this.server.sendToAllClients(msg_send); // send message to all clients
-			} catch (Exception e) {
-				System.err.println("[system] there was a problem while sending the message to all clients");
+				String tip = jsonObject.get("tipSporocila").toString();
+				String sporocilo = jsonObject.get("sporocilo").toString();
+				if (tip.equals("privatno")) {
+					if (!jsonObject.containsKey("za")) {
+						server.sporociNapako(socket, "manjka prejemnik");
+						continue;
+					}
+					String za = jsonObject.get("za").toString();
+					server.posljiPrivatnoSporocilo(socket, name, za, sporocilo);
+					System.out.println("pošiljanje privatnega sporočila od " + name);
+					continue;
+				}
+				else if (tip.equals("javno")) {
+					server.posljiJavnoSporocilo(name, sporocilo);
+					System.out.println(name + " => " + sporocilo);
+				}
+				else {
+						server.sporociNapako(socket, "napačen tip sporočila");
+						System.out.println(name + ": <napačen tip sporočila>");
+						continue;
+				}
+			}
+			catch (Exception e) {
+				System.out.println("Prišlo je do napake");
 				e.printStackTrace(System.err);
-				continue;
 			}
 		}
 	}
